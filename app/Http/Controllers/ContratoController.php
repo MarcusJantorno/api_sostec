20e05901a450f11e7e902d101670bc45e0c9a01d
Marcus Jantorno <marcusjantorno@gmail.com>
2023-03-13 13:18:12 -0300

    add3

--- app/Http/Controllers/ContratoController.php
+++ app/Http/Controllers/ContratoController.php
@@ -18,9 +18,7 @@ class ContratoController extends Controller
         $contratos = \DB::table('contratos')
             ->join('clientes', 'contratos.cliente_id', '=', 'clientes.id')
             ->join('cobrancas_geradas', 'cobrancas_geradas.contrato_id', '=', 'contratos.id')
-            ->select('contratos.*', 'clientes.nome', function ($query) {
-                $query->select(\DB::raw('MAX(cobrancas_geradas.vencimento) AS ultimo_vencimento'));
-            })
+            ->select('contratos.*', 'clientes.nome', \DB::raw('MAX(cobrancas_geradas.vencimento) AS ultimo_vencimento'))
             ->get();
         //$contratos = Contrato::all();
         return $contratos;
