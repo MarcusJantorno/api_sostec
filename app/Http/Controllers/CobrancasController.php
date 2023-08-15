978901f17b8411e2bcc36c5581327a14ae5b2c02
Marcus Jantorno <marcusjantorno@gmail.com>
2023-08-15 12:39:43 -0300

    alter

--- app/Http/Controllers/CobrancasController.php
+++ app/Http/Controllers/CobrancasController.php
@@ -18,7 +18,7 @@ class CobrancasController extends Controller
         $query = '
         select c.valor as valor, cg.cpf as cpf, c.contrato_id, cg.nome as cliente_nome, ce.descricao as contaefi, max(cg.vencimento) as ultimo_vencimento,
         case when cg.vencimento < CURDATE() and cg.status <> "CONCLUIDA" then 1
-        else 0
+        else null
         end as inadimplente,
         SUBSTRING(max(cg.vencimento), 9,10) as dia_venc
         from cobrancas c
