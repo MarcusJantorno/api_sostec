e2643bba2e9e555718f533a61a565c97ba5f2e91
Marcus Jantorno <marcusjantorno@gmail.com>
2024-01-04 23:44:58 -0300

    ALTER COB

--- app/Http/Controllers/CobrancasController.php
+++ app/Http/Controllers/CobrancasController.php
@@ -46,10 +46,10 @@ class CobrancasController extends Controller
                 MAX(CASE WHEN MONTH(cg.vencimento_original) = MONTH(CURDATE()) - 1 THEN cg.valor END)
             ) AS valor, cg.cpf as cpf, c.contrato_id, cg.nome as cliente_nome, ce.descricao as contaefi, max(cg.vencimento) as ultimo_vencimento, cc.estado as uf,
             MAX(CASE
-                WHEN cg.vencimento_original < CURDATE() AND cg.status <> 'CONCLUIDA' AND CURDATE() > cg.vencimento_original THEN
+                WHEN cg.vencimento_original < CURDATE() THEN
                     CASE
-                        WHEN DATEDIFF(CURDATE(), cg.vencimento_original) <= 2 THEN 2
-                        ELSE 3
+                        WHEN DATEDIFF(CURDATE(), cg.vencimento_original) <= 2 AND cg.status = 'ATIVA' THEN 2
+                        WHEN DATEDIFF(CURDATE(), cg.vencimento_original) > 2 AND cg.status = 'ATIVA' THEN 3
                     END
                 ELSE NULL
             END) AS inadimplente,
